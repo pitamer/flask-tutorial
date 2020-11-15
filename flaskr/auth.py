fad08a0716baaeafd348e318b0206d9ba00613c3
Pita <itamarga@gmail.com>
2020-11-15 11:29:12 +0200

    Add a first view to the app: signup

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,3 +8,33 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/signup', method=('GET', 'POST'))
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db
+        error = None
+
+        if not username:
+            error = 'no username'
+        elif not password:
+            error = 'no password'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'user already exists'
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/signup.html')
